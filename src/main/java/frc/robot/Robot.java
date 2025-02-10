304c7dab4132fe18fa9af5f5b65b99da201056be
MicahMaphet <micahmaphet@gmail.com>
2025-02-10 15:40:01 -0800

    working trigger readout

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,8 +14,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.swerve.Swerve;
@@ -66,8 +66,7 @@ public class Robot extends TimedRobot {
         );
       }
 
-      // Empty shifter command for l1
-      new Trigger(() -> shifter.getRawButton(0)).onTrue(Commands.none());
+      new Trigger(() -> shifter.getRawButton(0)).onTrue(new PrintCommand("shifted"));
   }
 
   /**
