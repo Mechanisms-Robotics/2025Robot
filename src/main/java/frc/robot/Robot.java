4a461a5abc6a10ed64273a25096d9a5edd2afc78
MicahMaphet <micahmaphet@gmail.com>
2025-02-18 23:24:27 -0800

    basic algae mechanism template

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.subsystems.algaemech.AlgaeMech;
 import frc.robot.subsystems.swerve.Swerve;
 
 /**
@@ -26,6 +27,7 @@ public class Robot extends TimedRobot {
   private final SendableChooser<Command> autoChooser = new SendableChooser<>();
 
   Swerve swerve = new Swerve();
+  AlgaeMech algaeMech = new AlgaeMech();
   Test test = new Test();
 
   CommandPS4Controller ps4Controller = new CommandPS4Controller(0);
@@ -60,6 +62,8 @@ public class Robot extends TimedRobot {
            ), swerve)
         );
       }
+
+      ps4Controller.R1().whileTrue(new RunCommand(algaeMech::intake, algaeMech));
   }
 
   /**
